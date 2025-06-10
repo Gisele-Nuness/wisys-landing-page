89a47f5cb1f76ba43472d8e489db8b6d64f09006
esthernascimento <esthersolzinhacarambola@gmail.com>
2025-06-09 23:22:57 -0300

    alteração nos cards de integrantes e parceiros / transição no header

--- src/js/index.js
+++ src/js/index.js
@@ -29,3 +29,14 @@ function type() {
   }
 }
 window.addEventListener('DOMContentLoaded', type);
+
+
+window.addEventListener("scroll", function () {
+  const navbar = document.getElementById("navbar");
+
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
