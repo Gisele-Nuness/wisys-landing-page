a7fd09edc833efdd3732db3f950d0f7403457121
marilimma <marinalimma10@gmail.com>
2025-10-21 16:40:51 -0300

    falta estilização do email

--- sendEmail.php
+++ sendEmail.php
@@ -27,16 +27,53 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->setFrom($email, $nome); 
         $mail->addAddress('admwisys@gmail.com'); 
 
+        
+        $template = "
+        <html>
+        <head>
+
+        <style>
+        body {font-family: Arial, sans-serif; }
+            .header { background-color: #0c31acff; padding: 10px; }
+            .content { margin: 20px; color:black; align-items:center; border:solid 2px #0c31acff; box-shaddow: ; }
+
+
+        </style>
+    </head>
+
+    <body>
+    <div class='header'>
+    <h2>Nova Mensagem recebida</h2>
+    </div>
+    <div class='content'>
+        <p><strong>Nome:</strong>{$nome}</p>
+        <p><strong>Email:</strong>{$email}</p>    
+        <p><strong>Mensagem:</strong></p>
+        <p>{$mensagem}</p>
+        </div>
+    </body>
+    </html>
+    ";
+
         $mail->isHTML(true);
         $mail->Subject = 'Nova mensagem';
-        $mail->Body    = "<strong>Nome:</strong> $nome<br>
-                          <strong>Email:</strong> $email<br>
-                          <strong>Mensagem:</strong><br>$mensagem";
+        $mail->Body    = "$template";
+
+
+
 
             $mail->send();
         echo 'Mensagem enviada com sucesso!';
     } catch (Exception $e) {
         echo "Erro ao enviar: {$mail->ErrorInfo}";
+
+        
     }
+
+
+     
+
+
+
 }
 ?>
